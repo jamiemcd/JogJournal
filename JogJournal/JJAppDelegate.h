f54943d65b01e840f425b1dd3230c04047714d11
Jamie McDaniel <jamie@curiousfind.com>
2013-12-17 10:52:10 -0500

    Initial Commit

---
+++ JogJournal/JJAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  JJAppDelegate.h
+//  JogJournal
+//
+//  Created by Jamie McDaniel on 12/17/13.
+//  Copyright (c) 2013 Curious Find. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ JogJournal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JogJournal
+//
+//  Created by Jamie McDaniel on 12/17/13.
+//  Copyright (c) 2013 Curious Find. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JJAppDelegate class]));
+    }
+}
